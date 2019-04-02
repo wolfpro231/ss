275a10c9326770354ff6b37cc25a34a09ecb927c
wolfpro231 <48035876+wolfpro231@users.noreply.github.com>
2019-04-02 22:56:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "+";
+var prefix = "Lst";
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Cyhper Bot Script By : DREAM`);
@@ -8,7 +8,8 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(` كما قال الشاعر كس ام المشاعر
+`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
